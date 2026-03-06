eab7cafb34e47af0f92b394e86b0b182e55bfe87
Nikolay Nosyrev <n.nosyrev@mail.ru>
2026-03-06 21:13:11 +0300

    Empty test for PPU

---
+++ tests/Unit/PPU/PPUTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\PPU;
+
+use PHPUnit\Framework\TestCase;
+
+// TODO: Tests for PPU
+final class PPUTest extends TestCase
+{
+    public function test(): void
+    {
+        $this->assertSame(true, false);
+    }
+}
