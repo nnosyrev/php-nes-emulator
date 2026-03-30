3a7bfd45d077afe1f23cba8697827568683e621e
nnosyrev <670872+nnosyrev@users.noreply.github.com>
2026-03-30 21:35:55 +0300

    Test for ASR opcode

---
+++ tests/Integration/CPU/Instruction/ASRTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Integration\CPU\Instruction;
+
+use PHPUnit\Framework\TestCase;
+use Tests\CPUTestTrait;
+
+final class ASRTest extends TestCase
+{
+    use CPUTestTrait;
+
+    public function testASR(): void
+    {
+        $this->loadProgramToRom([0xA9, 0b11111111, 0x4B, 0b00001111, 0x00]);
+
+        $CPU = $this->getCpu();
+        $CPU->run();
+
+        $this->assertSame($CPU->getRegisterA()->value, 0b00000111);
+        $this->assertSame($CPU->getFlagZ(), false);
+        $this->assertSame($CPU->getFlagC(), true);
+        $this->assertSame($CPU->getFlagN(), $this->getFlagNValue($CPU->getRegisterA()));
+    }
+}
