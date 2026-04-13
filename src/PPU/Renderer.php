a671c136fec86c2085f547cb7f802bd16b78ec71
nnosyrev <670872+nnosyrev@users.noreply.github.com>
2026-04-13 14:47:15 +0300

    Optimizations

--- src/PPU/Renderer.php
+++ src/PPU/Renderer.php
@@ -11,21 +11,28 @@ use App\UI\UIInterface;
 
 final class Renderer
 {
+    private array $pallete;
+
+    private Frame $frame;
+
     public function __construct(
         private readonly UIInterface $ui,
         private readonly RomInterface $rom,
-    ) {}
-
-    public function render(PPU $ppu): void
-    {
-        $pallete = [
+    ) {
+        $this->pallete = [
             new Rgb(new UInt8(50), new UInt8(100), new UInt8(200)),
             new Rgb(new UInt8(200), new UInt8(100), new UInt8(50)),
             new Rgb(new UInt8(100), new UInt8(50), new UInt8(200)),
             new Rgb(new UInt8(100), new UInt8(100), new UInt8(100)),
         ];
 
-        $frame = new Frame(new Rgb(new UInt8(255), new UInt8(255), new UInt8(255)));
+        $this->frame = new Frame(new Rgb(new UInt8(255), new UInt8(255), new UInt8(255)));
+    }
+
+    public function render(PPU $ppu): void
+    {
+        $chrRom = $this->rom->getChrRom();
+        $frame = $this->frame;
 
         // Render background
         $backgroundBankStart = $ppu->getBackgroundChrBank() * 0x1000;
@@ -35,21 +42,21 @@ final class Renderer
             $offset = $i - 0x2000;
             $tileN = $vram[$offset];
 
-            $tile = array_slice($this->rom->getChrRom(), $backgroundBankStart + $tileN * 16, 16);
+            $tile = array_slice($chrRom, $backgroundBankStart + $tileN * 16, 16);
 
             $baseX = ($offset % 32) * 8;
             $baseY = (int) floor($offset / 32) * 8;
 
-            foreach (range(0, 7) as $y) {
+            for ($y = 0; $y <= 7; $y++) {
                 $upper = $tile[$y];
                 $lower = $tile[$y + 8];
 
-                foreach (range(7, 0) as $x) {
+                for ($x = 7; $x >= 0; $x--) {
                     $value = (1 & $upper) << 1 | (1 & $lower);
                     $upper = $upper >> 1;
                     $lower = $lower >> 1;
 
-                    $frame->setPixel($baseX + $x, $baseY + $y, $pallete[$value]);
+                    $frame->setPixel($baseX + $x, $baseY + $y, $this->pallete[$value]);
                 }
             }
         }
@@ -65,76 +72,22 @@ final class Renderer
             $attributes = $oamData[$i + 2];
             $baseX = $oamData[$i + 3];
 
-            $tile = array_slice($this->rom->getChrRom(), $spriteBankStart + $tileN * 16, 16);
+            $tile = array_slice($chrRom, $spriteBankStart + $tileN * 16, 16);
 
-            foreach (range(0, 7) as $y) {
+            for ($y = 0; $y <= 7; $y++) {
                 $upper = $tile[$y];
                 $lower = $tile[$y + 8];
 
-                foreach (range(7, 0) as $x) {
+                for ($x = 7; $x >= 0; $x--) {
                     $value = (1 & $upper) << 1 | (1 & $lower);
                     $upper = $upper >> 1;
                     $lower = $lower >> 1;
 
-                    $frame->setPixel($baseX + $x, $baseY + $y, $pallete[$value]);
+                    $frame->setPixel($baseX + $x, $baseY + $y, $this->pallete[$value]);
                 }
             }
         }
 
         $this->ui->render($frame);
     }
-
-    /*
-    public function render(PPU $ppu): void
-    {
-        $pallete = [
-            new Rgb(new UInt8(50), new UInt8(100), new UInt8(200)),
-            new Rgb(new UInt8(200), new UInt8(100), new UInt8(50)),
-            new Rgb(new UInt8(100), new UInt8(50), new UInt8(200)),
-            new Rgb(new UInt8(100), new UInt8(100), new UInt8(100)),
-        ];
-
-        $frame = $this->getFrame(0, $pallete);
-
-        $this->ui->render($frame);
-    }
-
-    private function getFrame(int $bank, array $pallete): Frame
-    {
-        if ($bank > 1 || $bank < 0) {
-            throw new \Exception('Incorrect bank');
-        }
-
-        $frame = new Frame(new Rgb(new UInt8(255), new UInt8(255), new UInt8(255)));
-
-        if (count($this->rom->getChrRom()) === 0) {
-            // CHR ROM is empty
-            return $frame;
-        }
-
-        $bankStart = $bank * 0x1000;
-
-        foreach (range(0, 255) as $tileN) {
-            $tile = array_slice($this->rom->getChrRom(), $bankStart + $tileN * 16, 16);
-
-            $baseX = ($tileN % 32) * 8;
-            $baseY = (int) floor($tileN / 32) * 8;
-
-            foreach (range(0, 7) as $y) {
-                $upper = $tile[$y];
-                $lower = $tile[$y + 8];
-
-                foreach (range(7, 0) as $x) {
-                    $value = (1 & $upper) << 1 | (1 & $lower);
-                    $upper = $upper >> 1;
-                    $lower = $lower >> 1;
-
-                    $frame->setPixel($baseX + $x, $baseY + $y, $pallete[$value]);
-                }
-            }
-        }
-
-        return $frame;
-    }
-    */
 }
