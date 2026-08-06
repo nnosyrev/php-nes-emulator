c442fb0a4343cfa6049e6cde3cbb168c59acad9e
nnosyrev <670872+nnosyrev@users.noreply.github.com>
2026-08-06 13:46:15 +0300

    Complex test

--- tests/Integration/CPU/ComplexCPUTest/ComplexCPUTest.php
+++ tests/Integration/CPU/ComplexCPUTest/ComplexCPUTest.php
@@ -123,7 +123,7 @@ final class ComplexCPUTest extends TestCase
 
     public static function getData(): Generator
     {
-        $items = Items::fromFile(self::DIR . '/20.json');
+        $items = Items::fromFile(self::DIR . '/8b.json');
 
         foreach ($items as $item) {
             $name = $item->name . ' ' . \microtime();
